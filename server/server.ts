c1fdfc5b324832a73eb6f91196adef5b435a6436
ayanpatel98 <ayanpatel_98@yahoo.com>
2025-08-29 12:10:47 -0700

    set payload limit

--- server/server.ts
+++ server/server.ts
@@ -11,7 +11,7 @@ const app: express.Application = express();
 const PORT: number = parseInt(process.env.PORT || '5000');
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // Setup student-related routes
 setupStudentRoutes(app);
